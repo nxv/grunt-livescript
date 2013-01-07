b4947bdce70a7fdeb9a641ff53c0430f0c7f3b63
David Souther <davidsouther@gmail.com>
2013-01-07 18:27:55 -0500

    Cleaned up tests.

--- test/livescript_test.js
+++ test/livescript_test.js
@@ -5,6 +5,8 @@ exports.livescript = {
   compile: function(test) {
     'use strict';
 
+    test.expect(2);
+
     var actual = grunt.file.read('tmp/livescript.js');
     var expected = grunt.file.read('test/expected/livescript.js');
     test.equal(expected, actual, 'should compile livescript to javascript');
@@ -12,21 +14,6 @@ exports.livescript = {
     actual = grunt.file.read('tmp/concat.js');
     expected = grunt.file.read('test/expected/concat.js');
     test.equal(expected, actual, 'should compile multiple livescript files to a single javascript file');
-
-    actual = fs.readdirSync('tmp/individual').sort();
-    expected = fs.readdirSync('test/expected/individual').sort();
-    test.deepEqual(expected, actual, 'should individually compile files');
-
-    test.done();
-  },
-  flatten: function(test) {
-    'use strict';
-
-    test.expect(1);
-
-    var actual = fs.readdirSync('tmp/individual_flatten').sort();
-    var expected = fs.readdirSync('test/expected/individual_flatten').sort();
-    test.deepEqual(expected, actual, 'should individually compile files (to flat structure)');
 
     test.done();
   }
