30340864b76f536301ad0231b9575f7c94bbeff9
David Souther <davidsouther@gmail.com>
2013-02-20 10:27:12 -0500

    Moved to Grunt 0.4 for building.

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,9 +74,8 @@ module.exports = function(grunt) {
 
   // Whenever the 'test' task is run, first clean the 'tmp' dir, then run this
   // plugin's task(s), then test the result.
-  grunt.renameTask('test', 'nodeunit');
-  grunt.registerTask('test', 'clean livescript nodeunit');
+  grunt.registerTask('test', ['clean', 'livescript', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask('default', ['jshint', 'test']);
 };
